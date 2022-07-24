89f4a360710a4263a7185abe424b044b1105a8f3
Ray-of-light2020 <a.ray.of.light2020@gmail.com>
2022-07-24 14:20:00 +0900

    LogController -r の作成

---
+++ database/migrations/2022_07_24_103733_create_log_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_condition', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('size');
+            $table->float('thickness');
+            $table->string('shooting_method');
+            $table->float('ci');
+            $table->integer('sfd');
+            $table->float('irradiation_time');
+            $table->float('d_u');
+            $table->float('d_l');
+            $table->string('film');
+            $table->integer('deverop_time');
+            $table->integer('deverop_temp');
+            $table->string('remarks');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('log', 'LogController', ['only' => [
+    'index', 'store','edit','destroy'
+]]);
