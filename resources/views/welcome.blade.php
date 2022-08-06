54557ce1f79ede5f7ab8ff6e99e1c3e585fd3f16
Ray-of-light2020 <a.ray.of.light2020@gmail.com>
2022-08-06 10:11:13 +0900

    wellcomeページのrouteをurlに変更。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,10 +28,10 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
+                        <a href="{{ url('/login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">ユーザー登録</a>
+                            <a href="{{ url('/register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">ユーザー登録</a>
                         @endif
                     @endauth
                 </div>
